74ba9b069e4710ba2a25222a68bccd1c1480dd87
viveks5898 <singh.vivek5898@gmail.com>
2024-08-11 10:30:40 +0530

    get users

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import data from "@/app/data/users.json";
+
+export async function GET() {
+
+
+ 
+    return NextResponse.json({
+      data: data,
+      status: 200,
+
+    });
+}
